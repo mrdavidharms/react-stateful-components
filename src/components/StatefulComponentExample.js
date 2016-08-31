ee9b6de4f26fe4d71816aed9be37edd1d61d1993
David Harms <davidharms@Davids-MacBook-Pro-2.local>
2016-08-31 13:36:44 -0400

    initial

---
+++ src/components/StatefulComponentExample.js
@@ -0,0 +1,12 @@
+// StatefulComponentExample.js
+import React from 'react';
+
+const StatefulComponentExample = props => {
+  return (
+    <div>
+      <h1>Component Message: {props.message}</h1>
+    </div>
+  );
+};
+
+export default StatefulComponentExample;
---
+++ src/main.js
@@ -0,0 +1,9 @@
+// main.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import StatefulComponentExample from './components/StatefulComponentExample';
+
+ReactDOM.render(
+  <StatefulComponentExample message="hi" />,
+  document.getElementById('app')
+);
