f399a4099faa8c3619df02f486792d69aed5dbea
David Harms <davidharms@Davids-MacBook-Pro-2.local>
2016-09-01 14:19:16 -0400

    first-finished

--- src/components/StatefulComponentExample.js
+++ src/components/StatefulComponentExample.js
@@ -1,12 +1,31 @@
 // StatefulComponentExample.js
 import React from 'react';
 
-const StatefulComponentExample = props => {
-  return (
-    <div>
-      <h1>Component Message: {props.message}</h1>
-    </div>
-  );
+class StatefulComponentExample extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clickCount: 0,
+      chillLevel: 'V V chill'
+    };
+
+  this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    let newClickCount = this.state.clickCount + 1;
+    this.setState({ clickCount: newClickCount});
+  }
+
+  render() {
+    return(
+      <div onClick={this.handleClick}>
+        <h1>Component Message: {this.props.message}</h1>
+        <h1>Component Click Count: {this.state.clickCount}</h1>
+        <h1>Component Chill Level: {this.state.chillLevel}</h1>
+      </div>
+    );
+  }
 };
 
 export default StatefulComponentExample;
